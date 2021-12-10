510b332c4a84f4c78c5880f81712a51ffc3795a7
Anddres23 <94419992+Anddres23@users.noreply.github.com>
2021-12-09 21:12:50 -0500

    Update netsize.sh
    correccion

--- netsize.sh
+++ netsize.sh
@@ -1,5 +1,5 @@
 
-# Saavedra y Stouffer (2013) estudiaron varias redes planta-polinizador. Estos se pueden representar como matrices rectangulares
+Saavedra y Stouffer (2013) estudiaron varias redes planta-polinizador. Estos se pueden representar como matrices rectangulares
 donde las filas son polinizadores, las columnas plantas, un 0 indica la ausencia y 1 la presencia de una interacción entre la 
 planta y el polinizador. Los datos de Saavedra y Stouffer (2013) se pueden encontrar en el directorio CSB / unix / data / Saavedra2013. 
 
